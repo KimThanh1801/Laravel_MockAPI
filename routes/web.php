aa0c9ae63be912ace9c8bcd0a6bc19044905faed
KimThanh1801 <hokimthanh1234@gmail.com>
2025-02-25 08:07:51 +0700

    Done

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShowController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ProductController::class)
+    ->name('products.')
+    ->prefix('products')
+    ->group(function () {
+        // Danh sách sản phẩm
+        Route::get('/', 'index')->name('index');
+        // Hiển thị form thêm sản phẩm
+        Route::get('/create', 'create')->name('create');
+        // Xử lý lưu sản phẩm mới
+        Route::post('/store', 'store')->name('store');
+        // Hiển thị form chỉnh sửa sản phẩm
+        Route::get('/{id}/edit', 'edit')->name('edit');
+        // Cập nhật sản phẩm
+        Route::put('/{id}', 'update')->name('update');
+        // Xóa sản phẩm
+        Route::delete('/{id}', 'destroy')->name('destroy');
+    });
+
+    Route::get('/productss', [ShowController::class, 'index'])->name('productss.index');
