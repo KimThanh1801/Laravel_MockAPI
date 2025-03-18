f952215d5bc71b2a750ee79adcec98439796205d
KimThanh1801 <hokimthanh1234@gmail.com>
2025-03-18 09:07:21 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -6,25 +6,9 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShowController;
 use App\Http\Controllers\SlideController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::controller(ProductController::class)
-//     ->name('products.')
-//     ->prefix('products')
-//     ->group(function () {
-//         // Danh sách sản phẩm
-//         Route::get('/', 'index')->name('index');
-//         // Hiển thị form thêm sản phẩm
-//         Route::get('/create', 'create')->name('create');
-//         // Xử lý lưu sản phẩm mới
-//         Route::post('/store', 'store')->name('store');
-//         // Hiển thị form chỉnh sửa sản phẩm
-//         Route::get('/{id}/edit', 'edit')->name('edit');
-//         // Cập nhật sản phẩm
-//         Route::put('/{id}', 'update')->name('update');
-//         // Xóa sản phẩm
-//         Route::delete('/{id}', 'destroy')->name('destroy');
-//     });
 
 Route::get('/',function(){
     return view ('/welcome');
@@ -42,12 +26,6 @@ Route::get('/chitiet_sanpham/{id}', [PageController::class, 'chitietsanpham'])->
 Route::get('/detail/{id}',[PageController::class, 'getDetail'])->name('chitiet_sanpham');
 Route::post('/comment/{id}', [PageController::class, 'getDetail'])->name('comment.store');
 
-// Route::prefix('admin')->group(function () {
-//     Route::get('/', [AdminController::class, 'admin'])->name('admin.dashboard');
-//     Route::get('/edit/{id}', [AdminController::class, 'getAdminEdit'])->name('admin.edit');
-//     Route::post('/delete/{id}', [AdminController::class, 'postAdinDelete'])->name('admin.delete');
-// });
-
 Route::get('/admin',[PageController::class,'getIndexAdmin']);
 Route::get('/admin/edit/{id}', [PageController::class, 'getAdminEdit'])->name('getadminedit');
 // Route::post('/admin/add-product', [PageController::class, 'postAdminAdd'])->name('add-product');
@@ -63,3 +41,6 @@ Route::get('/export', [ExportController::class, 'export'])->name('export');
 
 Route::get('/admin/add-product', [PageController::class, 'showAddProductForm'])->name('add-product.form');
 Route::post('/admin/add-product', [PageController::class, 'postAdminAdd'])->name('add-product');
+
+Route::get('/login',[UserController::class,'getLogin']);
+Route::get('/singup',[UserController::class,'singup']);
